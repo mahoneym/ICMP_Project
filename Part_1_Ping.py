9a7b01726516b41789cc393c49b47ffa365d8a64
maggiemahoney <mahoneym6@xavier.edu>
2019-11-03 19:12:45 -0500

    part_1
    On part 1, I added some comments and created a text file with my answers for the project.

--- Part_1_Ping.py
+++ Part_1_Ping.py
@@ -3,9 +3,9 @@ import shlex
 
 cmd   = "ping -c 1 www.cs.xu.edu"           # saves the command in a variable
 
-args = shlex.split(cmd)                     # parses cmd to get it ready for the check_call
+args = shlex.split(cmd)                     # parses cmd to get it ready for the check_call method
 try:
     subprocess.check_call(args,stdout=subprocess.PIPE,stderr=subprocess.PIPE)
-    print ("Xavier CS web server is up!")
-except subprocess.CalledProcessError:
+    print ("Xavier CS web server is up!")       # prints that the ping was received
+except subprocess.CalledProcessError:           # if CalledProcessError exception, the ping wasn't received
     print ("Failed to get ping.")               # prints failed ping
